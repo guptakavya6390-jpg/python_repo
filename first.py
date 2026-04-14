583445a5940a57b9c049d019f28827583939b50c
Kavya <guptakavya6390@gmail.com>
2026-04-14 23:31:38 +0530

    updated file

--- first.py
+++ first.py
@@ -1,1 +1,123 @@
-This is python file
+'''sher = "harsh bhaiya"
+
+ShreyianSchool ="students" #pascal case
+shreyianSchool ="student" #camel case
+sheriyans_school ="students" #snake case'''
+---------------------------------------------------
+'''b = True
+c = False'''
+----------------------------------------
+'''a = 55, 5.5, 5i
+print(type(a)) #output = int,float,complex'''
+
+'''age = int(input("what is your age?\n"))
+print("My age is",age)
+output =
+what is your age?
+23
+My age is 23'''
+
+''' a = 4
+b = 2
+print(a+b)6
+print(a-b)2
+print(a*b)8
+print(a/b)2.0
+print(a//b)2
+print(a**b)16
+
+#assignment operator
+# a =3
+# compond assignment
+# a += 3
+# a -= 3
+----------------------------------------------
+'age = int(input("what is your age?\n"))
+print("My age is",age)
+if(age>50):
+  print("valid")
+else:
+     print("Invalid")''''
+
+---------------------------------------------------
+'''money = int(input("Enter your money\n"))
+if money>50:
+    print("chocobar")
+elif money==20:
+    print("yeah")
+else:
+    print("Icecream")'''
+--------------------------------------------------
+'''num1 = int(input("Enter your first number\n"))
+num2 = int(input("Enter your second number\n"))
+if num1>num2:
+    print(num1)
+else:
+    print(num2)'''
+-----------------------------------------------
+''''gen1= input("Please Enter your Gender\n")
+if gen1=="male" or gen1 =="Male" or gen1=="m" or gen1=="M":
+    print("Good Morning Sir")
+elif gen1=="female" or gen1=="female" or gen1=="f" or gen1=="F":
+     print("Good morning mam")
+else:
+    print("invalid gender")'''
+-----------------------------------------
+    '''temp = int(input("Enter temperature in degree celcius\n"))
+if temp<0:
+    print("freezing cold")
+elif temp>=0 and temp<10:
+    print("very cold")
+elif temp>=10 and temp<20:
+    print("cold")
+elif temp>=20 and temp<30:
+    print("pleasent")
+elif temp>=30 and temp<40:
+    print("Hot")
+else:
+    print("very hot")'''
+---------------------------------------
+'''a = range(20,51,1)
+for i in a:
+    print(i)
+-------------------------
+a = range(16,1,-1)
+for i in a:
+     print(i)
+--------------------------------------------
+for table of 5
+for i in range(5,51,5):
+     print(i) 
+-------------------------------------------
+for printing any table 
+     
+ n = int(input("Which number do you want to print:-\n"))
+for i in range(n, (n*10+1),n):
+     print(i) 
+     
+--------------------------------------------------
+a="Kavyagupta"
+for i in range(11):
+     print(a[i])
+     
+------------------------------------------------
+a = "I am going to text her and i want to print this is upper method"
+for i in range(len(a)):
+    print(a[i]) 
+-----------------------------------------------------------------
+   a ="Kavya gupta"
+for i in range(1,21):
+     if i==10:
+         break/continue
+     print(i)
+------------------------------------------------
+num = int(input("Enter your number\n"))
+for i in range(num):
+     print("Hello world")
+--------------------------------------------------------
+n = int(input("Enter your number to print\n"))
+for i in range(1,11):
+     print(f"{n} * {i} = {n*i}")
+
+----------------------------------------------------
+ '''
